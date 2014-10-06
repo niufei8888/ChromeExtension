f40deb2a33b6ab788417d6c177b3b923d8e0762c
Fei Niu <fniu@yahoo-inc.com>
2014-10-05 20:39:58 -0700

    ignore list

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+
+$(document).ready(function(){
+    $("#foo_button").click(function() {
+        var curNode = $(this);
+        if (curNode.text() == "foo") {
+            curNode.text("bar");
+        } else {
+            curNode.text("foo");
+        }
+    });
+});
+
