ec7b53bc5d3b1531c7c44517c3b0036f40ff7288
Fei Niu <fniu@yahoo-inc.com>
2014-10-19 23:20:12 -0700

    use content script to modify the background color

---
+++ app/scripts/content.js
@@ -0,0 +1,6 @@
+console.log("Testing content script!");
+
+// always remember to wait until page fully loaded!
+//$(document).ready(function(){
+//    document.body.style.backgroundColor="green";
+//});
